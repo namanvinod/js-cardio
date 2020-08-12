74bb6c44721fd0527107162333803231d8e1f3bb
Naman Vinod <namanvinod1@gmail.com>
2020-08-12 22:11:44 +0530

    JS Cardio | Added Anagram Methods

--- cardio-1.js
+++ cardio-1.js
@@ -1,3 +1,5 @@
+// Reverse String
+
 function reverseStr(str) {
     return (str || '').trim().split('').reverse().join('')
 }
@@ -18,5 +20,104 @@ function reverseStr_Reduce(str) {
         )
 }
 
-exports.reverseStr = reverseStr
-exports.reverseStr_Reduce = reverseStr_Reduce
+function reverseStr_Spread(str) {
+    return [...(str || '').trim()].reverse().join('')
+}
+
+function reverseStr_Recursion(str) {
+
+}
+
+// Check Anagram
+
+isAnagram = (str1, str2) => {
+    if (!str1 || !str2 || str1.trim() === '' || str2.trim() === '')
+        return false
+    return getCharSet(str1) === getCharSet(str2)
+}
+
+isAnagram_Reduce = (str1, str2) => {
+    if (!str1 || !str2 || str1.trim() === '' || str2.trim() === '')
+        return false
+    return getCharSet_Reduce(str1) === getCharSet_Reduce(str2)
+}
+
+getCharSet = (str) => [...(str || '').trim().toLowerCase()].sort().join('')
+
+getCharSet_Reduce = (str) => {
+    return [...(str || '').trim().toLowerCase()]
+        .reduce(
+            (acc, curr) => [...acc, curr]
+            ,[]
+        )
+        .sort()
+        .join('')
+}
+
+function convertCase(caseToConvert) {
+
+}
+
+function maxOccurance(str) {
+
+}
+
+function longestWord(str) {
+
+}
+
+function flatArr(arr) {
+
+}
+
+// Split an array into chunked arrays of a specific length
+// ex. chunkArr([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
+function chunkArr(arr) {
+
+}
+
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+function letterChanges(str) {
+
+}
+
+// Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
+function addAll() {}
+
+function sumAllPrimes(num) {}
+
+// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+
+function seekAndDestroy() {}
+
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// ex.
+// a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+function sortByHeight() {}
+
+// missingLetters("abce") == "d"
+// missingLetters("abcdefghjklmno") == "i"
+// missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
+
+function missingLetters() {}
+
+// evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
+function evenOddSums() {}
+
+exports.reverseString = {
+    reverseStr: reverseStr,
+    reverseStr_Reduce: reverseStr_Reduce,
+    reverseStr_Spread: reverseStr_Spread
+}
+
+// exports.reverseStr = reverseStr
+// exports.reverseStr_Reduce = reverseStr_Reduce
+// exports.reverseStr_Spread = reverseStr_Spread
+
+exports.anagram = {
+    isAnagram: isAnagram,
+    isAnagram_Reduce: isAnagram_Reduce
+}
--- cardio-1.spec.js
+++ cardio-1.spec.js
@@ -1,44 +1,99 @@
 const cardio1 = require('./cardio-1')
 
-const reverseStr = cardio1.reverseStr
-const reverseStr_Reduce = cardio1.reverseStr_Reduce
+const reverseString = cardio1.reverseString
+const anagram = cardio1.anagram
+
+// const reverseStr = cardio1.reverseStr
+// const reverseStr_Reduce = cardio1.reverseStr_Reduce
+// const reverseStr_Spread = cardio1.reverseStr_Spread
+
 const reverseStrTests = (reverseStrFn) => {
     test(
-        'Reverses a string', 
-        () => {
-            expect(reverseStrFn('hEllo')).toBe('ollEh')
-        }
-    ),
+        'Reverses a string',
+        () => expect(reverseStrFn('hEllo')).toBe('ollEh')
+    )
     test(
-        'Returns empty string if input is null', 
-        () => {
-            expect(reverseStrFn(null)).toBe('')
-        }
-    ),
+        'Returns empty string if input is null',
+        () => expect(reverseStrFn(null)).toBe('')
+    )
     test(
-        'Returns empty string if input is undefined', 
-        () => {
-            expect(reverseStrFn(undefined)).toBe('')
-        }
-    ),
+        'Returns empty string if input is undefined',
+        () => expect(reverseStrFn(undefined)).toBe('')
+    )
     test(
-        'Reverses input string ignoring trailing or leading whitespaces', 
-        () => {
-            expect(reverseStrFn('  hello    ')).toBe('olleh')
-        }
+        'Reverses input string ignoring trailing or leading whitespaces',
+        () => expect(reverseStrFn('  hello    ')).toBe('olleh')
+    )
+}
+
+const anagramTests = (anagramFn) => {
+    test(
+        'checks anagram for two anagram strings',
+        () => expect(anagramFn('Asdf', 'fsad')).toBeTruthy()
+    )
+    test(
+        'checks anagram for two anagram strings with capital letters',
+        () => expect(anagramFn('AsDf', 'FsaD')).toBeTruthy()
+    )
+    test(
+        'checks anagram for two anagram strings with leading whitespace',
+        () => expect(anagramFn('AsDf', '    FsaD  ')).toBeTruthy()
+    )
+    test(
+        'checks anagram for two non-anagram strings',
+        () => expect(anagramFn('AsDfff', 'FsaD')).toBeFalsy()
+    )
+    test(
+        'checks anagram for two empty string',
+        () => expect(anagramFn('', '')).toBeFalsy()
+    )
+    test(
+        'checks anagram for two empty strings with whitespace',
+        () => expect(anagramFn('', '    ')).toBeFalsy()
+    )
+    test(
+        'checks anagram for string and null',
+        () => expect(anagramFn('AsDf', null)).toBeFalsy()
+    )
+    test(
+        'checks anagram for string and undefined',
+        () => expect(anagramFn('AsDf', undefined)).toBeFalsy()
+    )
+    test(
+        'checks anagram for null and undefined',
+        () => expect(anagramFn(null, undefined)).toBeFalsy()
+    )
+    test(
+        'checks anagram for two null',
+        () => expect(anagramFn(null, null)).toBeFalsy()
+    )
+    test(
+        'checks anagram for undefined and undefined',
+        () => expect(anagramFn(undefined, undefined)).toBeFalsy()
     )
 }
 
 describe(
     'Reverse String Test Suite using array methods',
-    () => {
-        reverseStrTests(reverseStr)
-    }
+    () => reverseStrTests(reverseString.reverseStr)
 )
 
 describe(
     'Reverse String Test Suite using reduce',
-    () => {
-        reverseStrTests(reverseStr_Reduce)
-    }
+    () => reverseStrTests(reverseString.reverseStr_Reduce)
+)
+
+describe(
+    'Reverse String Test Suite using spread operator',
+    () => reverseStrTests(reverseString.reverseStr_Spread)
+)
+
+describe(
+    'Are two strings anagram',
+    () => anagramTests(anagram.isAnagram)
+)
+
+describe(
+    'Are two strings anagram using reduce',
+    () => anagramTests(anagram.isAnagram_Reduce)
 )
